c10d3125f545677952129f733e82a5d23a1611cd
Zdeněk Gromnica <futuremillennium@gmail.com>
2019-09-06 14:46:07 +0200

    + MIDI field accepts input + serviceworker.js updates cache

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,8 @@
+var CACHE = 'helmholtz';
+
 self.addEventListener('install', function(e) {
 	e.waitUntil(
-		caches.open('helmholtz').then(function(cache) {
+		caches.open(CACHE).then(function(cache) {
 			var path = location.pathname.substring(0, location.pathname.lastIndexOf('/') + 1);
 			
 			return cache.addAll([path]);
@@ -8,11 +10,19 @@ self.addEventListener('install', function(e) {
 	);
 });
 
-
 self.addEventListener('fetch', function(event) {
 	event.respondWith(
 		caches.match(event.request).then(function(response) {
-			return response || fetch(event.request);
-		})
-	);
+			return response || fetch(event.request).then(function (response) {
+				return caches.open(CACHE).then(function (cache) {
+					return cache.put(event.request, response);
+				});
+			});
+		}));
+
+	event.waitUntil(fetch(event.request).then(function (response) {
+		return caches.open(CACHE).then(function (cache) {
+			return cache.put(event.request, response);
+		});
+	}));
 });
